7b9803b8b8b6b85e8e6473aca4b3d222092f6070
DiederichSolis <sol22952@uvg.edu.gt>
2024-09-24 22:28:29 -0600

    feat: creacion del proyecto

---
+++ minecraft/target/debug/deps/minecraft-93b98c409c58b7f6.d
@@ -0,0 +1,5 @@
+/Users/diederichsolis/Documents/carpeta sin título/ProyectoGRA/minecraft/target/debug/deps/libminecraft-93b98c409c58b7f6.rmeta: src/main.rs
+
+/Users/diederichsolis/Documents/carpeta sin título/ProyectoGRA/minecraft/target/debug/deps/minecraft-93b98c409c58b7f6.d: src/main.rs
+
+src/main.rs:
---
+++ minecraft/target/debug/deps/minecraft-f22ba255fd69cf6a.d
@@ -0,0 +1,5 @@
+/Users/diederichsolis/Documents/carpeta sin título/ProyectoGRA/minecraft/target/debug/deps/libminecraft-f22ba255fd69cf6a.rmeta: src/main.rs
+
+/Users/diederichsolis/Documents/carpeta sin título/ProyectoGRA/minecraft/target/debug/deps/minecraft-f22ba255fd69cf6a.d: src/main.rs
+
+src/main.rs:
